b760541c47722f3c39f7cd3041c572bfc0ecb70a
comwrg <xcomwrg@gmail.com>
2018-08-07 18:51:01 +0800

    modify log time format

--- log.js
+++ log.js
@@ -1,7 +1,7 @@
 const moment = require('moment')
 
 function d(s) {
-  process.stdout.write(moment().format('YYYY/MM/DD hh:mm:ss: ') + s.toString() + '\n')
+  process.stdout.write(moment().format('YYYY/MM/DD HH:mm:ss:SSS: ') + s.toString() + '\n')
 }
 
 module.exports = {
